8545e853ee119a461ce7f20cced8c7623bc6605d
Coutellier <33906647+RedDarkS@users.noreply.github.com>
2021-04-09 12:29:11 +0200

    oupsi clean 2ème

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -2,7 +2,7 @@ class Player extends Phaser.Physics.Arcade.Sprite
 {
     constructor(scene, x, y) 
     {
-        super(scene, x, y, "player_plus", "player_stance")
+        super(scene, x, y, "player_animes")
         scene.add.existing(this)
         scene.physics.add.existing(this)
 
