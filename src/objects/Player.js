806af27ac692feda5481ce488b812cd91124aef7
Coutellier <33906647+RedDarkS@users.noreply.github.com>
2021-04-09 12:55:38 +0200

    clean 3ème

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -94,11 +94,8 @@ class Player extends Phaser.Physics.Arcade.Sprite
         if(this._directionY < 0)
         {
             if(this.body.blocked.down
-                // || this.body.touching.down
                 || this.body.blocked.right
-                // || this.body.touching.right
                 || this.body.blocked.left
-                // || this.body.touching.left
             )
             {
                 this.setVelocityY(-550);
